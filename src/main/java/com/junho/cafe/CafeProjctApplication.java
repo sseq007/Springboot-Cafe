b31320d3db6ae4006467b3f60f2c59462def2cc6
sseq007 <sseq007@naver.com>
2022-11-04 02:41:54 +0900

    환경세팅 완료

---
+++ src/main/java/com/junho/cafe/CafeProjctApplication.java
@@ -0,0 +1,13 @@
+package com.junho.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeProjctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeProjctApplication.class, args);
+	}
+
+}
